6fceed7aadf3353de57146e6ab75b069f9cee653
henriquezcontrerasc@gmail.com <henriquezcontrerasc@gmail.com>
2026-04-06 17:03:15 -0500

    about us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './Componentes/home/home';
+import { About } from './Componentes/about/about';
+import { Features } from './Componentes/features/features';
+import { Plan } from './Componentes/plan/plan';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
-
+    { path: 'about', component: About },
+    { path: 'features', component: Features },
+    { path: 'plan', component: Plan }, 
+    
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
